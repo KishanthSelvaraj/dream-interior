93cb3d7ce5583b82c349fbf8f96bd51830439f06
KishanthSelvaraj <154578200+KishanthSelvaraj@users.noreply.github.com>
2024-04-24 14:39:22 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,7 +12,8 @@ import dinning from "/dinning.jpg";
 import kitchen from "/kitchen.jpg";
 import hall from "/hall.jpg";
 import axios from "axios";
-
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 import { MdKitchen } from "react-icons/md";
 import { MdTableRestaurant } from "react-icons/md";
 import { IoBagCheckOutline } from "react-icons/io5";
@@ -239,30 +240,44 @@ function Home() {
     setSubmitting(true); // Disable submit button while submitting
 
     try {
-      const response = await axios.post("http://localhost:3000/signup", user);
-      console.log("Response from server:", response.data); // Log the response from the server
+      const response = await axios.post("https://dream-interior-2-3.onrender.com/signup", user);
+      console.log("Response from server:", response.data); 
+      sendwhatsapp(); // Log the response from the server
+
       // You can add further actions for successful submission, e.g., showing a success message
     } catch (error) {
       console.error("Error submitting form:", error); // Log any errors
       // You can add further actions for error handling, e.g., showing an error message
     } finally {
-      setSubmitting(false); // Re-enable submit button after submission or error
+      setSubmitting(false);
+      // Re-enable submit button after submission or error
     }
   };
-  function sendwhatsapp(){
-    let phonenumber = "+916369729001";
-    let name = document.querySelector('#name').value;
-    let email = document.querySelector('#email').value;
-    let phone = document.querySelector('#phonenumber').value;
-    let message = document.querySelector('#message').value;
+  function sendwhatsapp() {
+    let phonenumber = "+917845780933";
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let phone = document.querySelector("#phonenumber").value;
+    let message = document.querySelector("#message").value;
 
-    let url = "https://wa.me/"+phonenumber+"?text="
-    +"Name :"+name+"%0a"
-    +"Email :"+email+"%0a"
-    +"Phone :"+phone+"%0a"
-    +"Message :"+message+"%0a%0a"
-    +"Thank you!!!";
-    window.open(url,'_blank').focus
+    let url =
+      "https://wa.me/" +
+      phonenumber +
+      "?text=" +
+      "Name :" +
+      name +
+      "%0a" +
+      "Email :" +
+      email +
+      "%0a" +
+      "Phone :" +
+      phone +
+      "%0a" +
+      "Message :" +
+      message +
+      "%0a%0a" +
+      "Thank you!!!";
+    window.open(url, "_blank").focus;
   }
   return (
     <div className="relative bg-transparent overflow-x-hidden">
@@ -289,7 +304,37 @@ function Home() {
             Your home should tell the story of who you are, and be a collection
             of what you love.
           </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
+          <div className="my-5">
+            <ul class="wrapper">
+              <li class="icon facebook">
+                <span class="tooltip">Facebook</span>
+                <span>
+                  <a href="https://www.facebook.com/profile.php?id=61550334992051&mibextid=LQQJ4d"><FaFacebook /></a>
+                </span>
+              </li>
+              {/* <li class="icon twitter">
+    <span class="tooltip">Twitter</span>
+    <span><i class="fab fa-twitter"></i></span>
+  </li> */}
+              <li class="icon instagram">
+                <span class="tooltip">Instagram</span>
+                <span>
+                  <a href="https://www.instagram.com/dream_interior23?igsh=dmpoa2Z5Y2d0OHRj&utm_source=qr">
+                    <FaInstagram />
+                  </a>
+                </span>
+              </li>
+              {/* <li class="icon github">
+    <span class="tooltip">Github</span>
+    <span><i class="fab fa-github"></i></span>
+  </li>
+  <li class="icon youtube">
+    <span class="tooltip">Youtube</span>
+    <span><i class="fab fa-youtube"></i></span>
+  </li> */}
+            </ul>
+          </div>
+          <div className=" flex items-center justify-center gap-x-6">
             <a
               href="#contact"
               className="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
@@ -536,7 +581,7 @@ function Home() {
             {/* <div className="flex justify-center"> */}
             <button
               type="submit"
-              onClick={sendwhatsapp}
+              // onClick={sendwhatsapp}
               className="py-3 px-4 text-sm font-medium text-white rounded-lg bg-gray-500 w-fit hover:bg-gray-600 focus:ring-4 focus:outline-none"
               disabled={submitting} // Disable button when submitting
             >
@@ -544,62 +589,188 @@ function Home() {
             </button>
             {/* </div> */}
           </form>
+
+          <div className="pt-5 text-center border-black flex justify-center items-center">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3890.191663459121!2d80.16330817512048!3d12.830888087471681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52593ec19dcc6b%3A0xd7d50c92e33d21c6!2sDream%20interior!5e0!3m2!1sen!2sin!4v1713883412557!5m2!1sen!2sin"
+              width="1000"
+              height="350"
+              style={{ border: "0" }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
-      </section>
-      {/* contact us end */}
-      <div className="w-full md:text-sm text-[13px] justify-center flex text-white bg-black">
+        </section>
+        {/* contact us end */}
+        {/* <div className="w-full md:text-sm text-[13px] justify-center flex text-white bg-black">
         Copyright © 2024. All rights reserved.
-      </div>
+      </div> */}
+        <footer className="bg-white text-center text-neutral-600 lg:text-left">
+          <div className="flex items-center justify-center border-b-2  p-6 lg:justify-between">
+            <div className="mr-12 hidden lg:block">
+              <span>Get connected with us on social networks:</span>
+            </div>
+            {/* <!-- Social network icons container --> */}
+            <div className="flex justify-center">
+              <a className="mr-6 text-neutral-600 " href="https://www.facebook.com/profile.php?id=61550334992051&mibextid=LQQJ4d">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+                </svg>
+              </a>
+             
+            
+              <a className="mr-6 text-neutral-600" href="https://www.instagram.com/dream_interior23?igsh=dmpoa2Z5Y2d0OHRj&utm_source=qr">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+
+          {/* <!-- Main container div: holds the entire content of the footer, including four sections (TW Elements, Products, Useful links, and Contact), with responsive styling and appropriate padding/margins. --> */}
+          <div className="mx-6 py-10 text-center md:text-left">
+            <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-2">
+              {/* <!-- TW Elements section --> */}
+              <div className="flex flex-col items-center justify-center">
+                <div className="flex mb-4 items-center justify-center">
+                  <img
+                    src="/dream-interior-logo.png"
+                    alt="dream interior logo"
+                    className="w-10 h-10 lg:w-20 lg:h-20 "
+                  />
+                  <a href="#" className=" p-1.5 pt-3 ">
+                    <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl text-gray-500 font">
+                      Dream Interior
+                    </h1>
+                  </a>
+                </div>
+                <p>
+                  Your home should tell the story of who you are, and be a
+                  collection of what you love.
+                </p>
+              </div>
+
+              {/* <!-- Products section --> */}
+              {/* <div className="">
+            <h6
+              className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Pages
+            </h6>
+            <p className="mb-4">
+              <a className="text-neutral-600 dark:text-neutral-200" href="/home"
+              >Home</a>
+            </p>
+            <p className="mb-4">
+              <a className="text-neutral-600 dark:text-neutral-200" href="/about"
+              >About</a>
+            </p>
+            <p className="mb-4">
+              <a className="text-neutral-600 dark:text-neutral-200" href="/services"
+              >Services</a>
+            </p>
+            <p>
+              <a className="text-neutral-600 dark:text-neutral-200" href="/contact"
+              >Contact</a>
+            </p>
+          </div> */}
+
+              {/* <!-- Contact section --> */}
+              <div>
+                <h6 className="mb-4 flex justify-center font-bold uppercase md:justify-start text-gray-500 text-xl ">
+                  {/* <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="mr-3 h-5 w-5">
+                <path
+                  d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                <path
+                  d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+              </svg> */}
+                  Contact
+                </h6>
+                <p className="mb-4 flex items-center justify-center md:justify-start">
+                  {/* <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="mr-3 h-5 w-5">
+                <path
+                  d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                <path
+                  d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+              </svg> */}
+                  No. 812, Pillayar Koil Street, Medavakkam Main Road,
+                  Mambakkam,Chengalpattu,Tamil nadu - 600127
+                </p>
+                <p className="mb-4 flex items-center justify-center md:justify-start">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="mr-3 h-5 w-5"
+                  >
+                    <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+                    <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+                  </svg>
+                  dreaminterior23@gmail.com
+                </p>
+                <p className="mb-4 flex items-center justify-center md:justify-start">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="mr-3 h-5 w-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  +91 7845780933
+                </p>
+                {/* <p className="flex items-center justify-center md:justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="mr-3 h-5 w-5">
+                <path
+                  fillRule="evenodd"
+                  d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
+                  clipRule="evenodd" />
+              </svg>
+              + 01 234 567 89
+            </p> */}
+              </div>
+            </div>
+          </div>
+
+          {/* <!--Copyright section--> */}
+          <div className="bg-gray-200 p-3 text-center ">
+            Copyright © 2024. All rights reserved.
+          </div>
+          {/* <div className="w-full md:text-sm text-[13px] justify-center flex text-white bg-black">
+        Copyright © 2024. All rights reserved.
+      </div> */}
+        </footer>
+    
     </div>
   );
 }
 
 export default Home;
-// import './App.css';
-// import React, { useState } from 'react';
-// import axios from 'axios'
-// function App() {
-//   const [user, setUser] = useState({
-//     username: "",
-//     password: ""
-//   });
-
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-//     setUser(prevUser => ({
-//       ...prevUser,
-//       [name]: value
-//     }));
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     axios.post('http://localhost:3000/signup',user)
-
-//   };
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <form onSubmit={handleSubmit}>
-//           <label>Name</label>
-//           <input
-//             type="text"
-//             name='username'
-//             value={user.username}
-//             onChange={handleInputChange}
-//           />
-//           <label>Pass</label>
-//           <input
-//             type="password"
-//             name='password'
-//             value={user.password}
-//             onChange={handleInputChange}
-//           />
-//           <button type="submit">Submit</button>
-//         </form>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
